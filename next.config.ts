758531fbb0dd32b8976eac7ec5a5e643713b0919
tgbypc <tugbaypc.11@gmail.com>
2025-08-15 21:18:47 +0200

    vercel update3

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
-// next.config.ts
-import type { NextConfig } from 'next'
+
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
-      { protocol: 'https', hostname: 'cdn.pixabay.com' },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "pixabay.com" }, // kullanıyorsan
+      { protocol: "https", hostname: "cdn.pixabay.com" }, // bazı pixabay görselleri
+      { protocol: "https", hostname: "blob.vercel-storage.com" }, // ileride upload için
     ],
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
